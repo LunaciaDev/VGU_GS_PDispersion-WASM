c82cd3c09acf4d82731b05dda4e783f2062a2974
Lunacia <130038743+LunaciaDev@users.noreply.github.com>
2026-02-04 05:48:54 +0000

    update benchmark program to take input instead of fixed input

--- examples/solve.rs
+++ examples/solve.rs
@@ -1,13 +1,26 @@
+use std::env;
+
 use p_dispersion::{Point, solve_p_dispersion_rs};
 
 fn main() {
-    let mut input = Vec::with_capacity(150);
+    let args: Vec<String> = env::args().collect();
 
-    for row in 0..10 {
-        for col in 0..15 {
+    if args.len() != 4 {
+        println!("Usage: solve row_count col_count select_count");
+        return;
+    }
+
+    let row_count = args[1].parse::<u32>().expect("row_count must be u32");
+    let col_count = args[2].parse::<u32>().expect("col_count must be u32");
+    let select_count = args[3].parse::<u32>().expect("select_count must be u32");
+
+    let mut input = Vec::with_capacity((row_count * col_count) as usize);
+
+    for row in 0..row_count {
+        for col in 0..col_count {
             input.push(Point::new(row as f32, col as f32));
         }
     }
 
-    println!("{:?}", solve_p_dispersion_rs(&input, 12).unwrap());
+    println!("{:?}", solve_p_dispersion_rs(&input, select_count).unwrap());
 }
